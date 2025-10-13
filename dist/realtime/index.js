9c8b3dd8ba1bfc5a00e61db0f1f4c2c6170e7ae5
Kunboruto20 <gyovanyy147@gmail.com>
2025-10-13 17:53:16 +0300

    Update index.js

--- dist/realtime/index.js
+++ dist/realtime/index.js
@@ -1,28 +1,72 @@
-const RealtimeWS = require('./websocket');
+const IgWebSocket = require('./websocket');
 
-class RealtimeRepository {
+class Realtime {
   constructor(client) {
     this.client = client;
-    this.ws = new RealtimeWS('wss://edge-mqtt.facebook.com', client);
+    this.ws = new IgWebSocket(client);
+
+    this.ws.on('message', (msg) => {
+      // Emit evenimente pe client
+      if (msg.type === 'direct_message') {
+        this.client.emit('direct_message', msg);
+      }
+      if (msg.type === 'thread_update') {
+        this.client.emit('thread_update', msg);
+      }
+      if (msg.type === 'story_update') {
+        this.client.emit('story_update', msg);
+      }
+    });
+
+    this.ws.on('connected', () => {
+      console.log('[Realtime] Connected to Instagram Realtime WebSocket');
+    });
+
+    this.ws.on('disconnected', () => {
+      console.log('[Realtime] Disconnected from Instagram Realtime WebSocket, reconnecting...');
+    });
+
+    this.ws.on('error', (err) => {
+      console.error('[Realtime] WebSocket error:', err);
+    });
   }
 
-  start() {
-    this.ws.connect();
+  async connect() {
+    await this.ws.connect();
   }
 
-  sendMessage(toUserId, message) {
+  async disconnect() {
+    await this.ws.disconnect();
+  }
+
+  sendDirectMessage(threadId, text) {
+    if (!this.ws.connected) throw new Error('WebSocket not connected');
     const payload = {
       type: 'direct_message',
-      recipient_id: toUserId,
-      text: message,
-      timestamp: Date.now(),
+      thread_id: threadId,
+      text,
     };
     this.ws.send(payload);
   }
 
-  onMessage(callback) {
-    this.ws.onMessage(callback);
+  sendReaction(threadId, reaction) {
+    if (!this.ws.connected) throw new Error('WebSocket not connected');
+    const payload = {
+      type: 'reaction',
+      thread_id: threadId,
+      reaction,
+    };
+    this.ws.send(payload);
+  }
+
+  markThreadSeen(threadId) {
+    if (!this.ws.connected) throw new Error('WebSocket not connected');
+    const payload = {
+      type: 'mark_seen',
+      thread_id: threadId,
+    };
+    this.ws.send(payload);
   }
 }
 
-module.exports = RealtimeRepository;
+module.exports = Realtime;
